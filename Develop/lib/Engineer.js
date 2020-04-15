ce097f6e2b825f21cbadc8dd07aa02eb8602d476
Sam Poppe <poppe.samuel@gmail.com>
2020-04-14 19:58:37 -0700

    modify all classes to pass tests

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,21 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name1, id1, email1, github) {   
+        const name = name1;
+        const id = id1;
+        const email = email1;
+
+        super(name, id, email);
+
+        this.github = github;
+        this.role = "Engineer";
+    }
+
+    getGithub() {
+        return this.github;
+    }
+
+}
+
+module.exports = Engineer;
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(name1, id1, email1, school) {
+        const name = name1;
+        const id = id1;
+        const email = email1;
+
+        super(name, id, email1);
+
+        this.school = school
+        this.role = "Intern";
+    }
+
+    getSchool(){
+        return this.school;
+    }
+}
+
+module.exports = Intern;
