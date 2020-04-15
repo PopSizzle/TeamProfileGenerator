c98208b0a6b50b808286d57aa0e5de12f7ed4ec8
Sam Poppe <poppe.samuel@gmail.com>
2020-04-14 21:28:13 -0700

    add inquirer prompts

--- Develop/app.js
+++ Develop/app.js
@@ -4,6 +4,8 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+let manager = {};
+let employees = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -11,7 +13,99 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
+function beginTeam () {
+
+    console.log("Welcome to the team creator!")
+    console.log("We will now begin to construct your team profile");
+    console.log("------------------------------------------")
+
+    enterTeam();
+}
+
+function enterTeam() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Please select an employee role to add.",
+            name: "role",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+            ]
+        },
+        {
+            type: "input",
+            message: "Please enter the name of your employee.",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter the id of your employee.",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter the email of your employee.",
+            name: "email"
+        }
+    ])
+    .then(function(response) {
+
+        let employee = {
+            role: response.role,
+            name: response.name,
+            id: response.id,
+            email: response.email
+        }
+
+        switch(response.role) {
+
+        case "Manager":
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Please enter your manager's office number.",
+                    name: "officeNumber"
+                }
+            ])
+            .then(function(response){
+                employee.officeNumber = response.officeNumber;
+            })
+            break;
+        case "Engineer":
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Please enter your Engineer's Github username.",
+                    name: "github"
+                }
+            ])
+            .then(function(response){
+                employee.github = response.github;
+            })
+            break;
+        default:
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Please enter your Intern's school.",
+                    name: "school"
+                }
+            ])
+            .then(function(response){
+                employee.school = response.school;
+            })
+        }
+
+        console.log(employee);
+    }   
+    )
+
+    enterTeam();
+}
+
+beginTeam();
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
